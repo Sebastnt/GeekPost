a46c267bc20789d7873ae0269e14cd57184fe86f
Sebastnt <sebastnt.p@hotmail.com>
2023-03-19 21:14:32 +0000

    First workaround db

--- src/api/models.py
+++ src/api/models.py
@@ -6,14 +6,47 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    website_url = db.Column(db.String(250), unique=True, nullable=True)
+    facebook_profile =  db.Column(db.String(250), nullable=True)
+    instagram_profile =  db.Column(db.String(250), nullable=True)
+    business_name =  db.Column(db.String(250), nullable=True)
+    post = db.relationship('Post', backref='user', lazy=True)
+    design = db.relationship('Design', backref='user', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_name = db.Column(db.String(250), nullable=True)
+    
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+        }
+
+class Design(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    design_name = db.Column(db.String(250), nullable=True)
+    
+    def __repr__(self):
+        return '<Design %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,87 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Post, Design
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+import json
 
 api = Blueprint('api', __name__)
 
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    if user is None:
+        return jsonify({"msg": "User doesn't exist"}), 404
+    if email != user.email or password != user.password:
+        return jsonify({"msg": "Bad email or password"}), 401
 
+    access_token = create_access_token(identity=user.id)
+    return jsonify(access_token=access_token)
+
+
+@api.route("/home", methods=["GET"])
+@jwt_required()
+def home():
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "msg": "Hello, you are logged in",
     }
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
+
+@api.route("/signup", methods=["POST"])
+def signup():
+    body = json.loads(request.data)
+    user = User.query.filter_by(email=body["email"]).first()
+
+    if user:
+        return jsonify({"msg": "User already exist"}), 404
+
+    new_user = User(
+        email = body["email"], 
+        password = body["password"],
+        full_name = body["full_name"])
+
+    db.session.add(new_user)
+    db.session.commit()
+    
+    response_body = {"msg": "Hello, you have signed up"}
     return jsonify(response_body), 200
+
+
+@api.route('/profile', methods=['DELETE'])
+@jwt_required()
+def delete_account():
+    current_user = get_jwt_identity()
+    user_query = User.query.filter(user_id==current_user).first()
+
+    if user_query:
+        db.session.delete(favorites_query)
+        db.session.commit()
+        return jsonify({"msg": "Your account has been deleted"}), 200
+    
+    if not favorites_query:
+        return jsonify({"msg": "Not able to delete this account"}), 200
+
+
+@api.route('/profile', methods=['PUT'])
+@jwt_required()
+def update_account():
+    current_user = get_jwt_identity()
+    user_query = User.query.filter(user_id==current_user).first()
+
+    body = json.loads(request.data)
+    new_user = User(
+        email = body["email"], 
+        password = body["password"],
+        full_name = body["full_name"])
+
+    db.session.add(new_user)
+    db.session.commit()
